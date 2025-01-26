4fdf97810ce8d24194abeb8a76f3b252c5a5b9ec
Limhax1 <ballaarontest0@gmail.com>
2025-01-26 21:18:32 +0100

    Initial commit

---
+++ src/main/java/me/limhax/pegasus/pegasusAC/PegasusAC.java
@@ -0,0 +1,17 @@
+package me.limhax.pegasus.pegasusAC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PegasusAC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
